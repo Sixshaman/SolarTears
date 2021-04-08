c69093aab97b25fbde749bdc303f14dda1ffd0cf
Roman <roman.sw@rangevision.com>
2021-04-08 15:06:49 +0300

    Base D3D12 renderer

---
+++ Sources/SolarTears/Rendering/D3D12/D3D12Utils.cpp
@@ -0,0 +1,6 @@
+#include "D3D12Utils.hpp"
+#include "..\..\Core\Util.hpp"
+#include "..\..\Logging\LoggerQueue.hpp"
+#include <string>
+#include <fstream>
+#include <cinttypes>
---
+++ Sources/SolarTears/Rendering/D3D12/D3D12Utils.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <d3d12.h>
+#include <string>
+#include <vector>
+#include "../../../3rd party/DirectXMath/Inc/DirectXMath.h"
+
+class LoggerQueue;
+
+namespace D3D12
+{
+	namespace D3D12Utils
+	{
+		//3 frames in flight is good enough
+		constexpr uint32_t InFlightFrameCount = 3;
+
+		template<typename T>
+		static constexpr DXGI_FORMAT FormatForVectorType = DXGI_FORMAT_UNKNOWN;
+
+		template<>
+		static constexpr DXGI_FORMAT FormatForVectorType<DirectX::XMFLOAT4> = DXGI_FORMAT_R32G32B32A32_FLOAT;
+
+		template<>
+		static constexpr DXGI_FORMAT FormatForVectorType<DirectX::XMFLOAT3> = DXGI_FORMAT_R32G32B32_FLOAT;
+
+		template<>
+		static constexpr DXGI_FORMAT FormatForVectorType<DirectX::XMFLOAT2> = DXGI_FORMAT_R32G32_FLOAT;
+
+		template<typename T>
+		static constexpr DXGI_FORMAT FormatForIndexType = DXGI_FORMAT_UNKNOWN;
+
+		template<>
+		static constexpr DXGI_FORMAT FormatForIndexType<uint32_t> = DXGI_FORMAT_R32_UINT;
+
+		template<>
+		static constexpr DXGI_FORMAT FormatForIndexType<uint16_t> = DXGI_FORMAT_R16_UINT;
+
+		template<>
+		static constexpr DXGI_FORMAT FormatForIndexType<uint8_t> = DXGI_FORMAT_R8_UINT;
+	}
+}
