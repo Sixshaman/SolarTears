5a3d5aa9d7dcdafbf02a7c7f156bc25360ddd827
Roman <roman.sw@rangevision.com>
2021-07-05 16:01:48 +0300

    Small change

--- Sources/SolarTears/Rendering/OGL/OGLUtils.cpp
+++ Sources/SolarTears/Rendering/OGL/OGLUtils.cpp
@@ -3,5 +3,5 @@
 
 void OpenGL::OGLUtils::GLDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
 {
-	Utils::SystemDebugMessage();
+	
 }
